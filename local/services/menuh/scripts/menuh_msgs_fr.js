f734f4c5a842fe65fe13136a8199da3b3782dc41
mat.martin <mat.martin@criteo.com>
2022-11-17 19:19:19 +0100

    try

--- local/services/menuh/scripts/menuh_msgs_fr.js
+++
@@ -1,8 +0,0 @@
-var menuh_message = {
-"desc" : "(...)",
-"navadmin" : "La navigation en mode admin s'effectue dans Pages/Menus.",
-"navadmin-node": '<div style="width: 450px">La navigation en mode admin s\'effectue dans Pages/Menus.</div><div style="padding-top: 1em">Pour déplacer le menu, utiliser la barre d\'outils.</div>'
-} ;
-
-var menuh_msgs_fr_js_loaded = true ;
-if ( window.qdyn_loaded ) qdyn_loaded ( "menuh_msgs_fr_js_loaded" ) ;
